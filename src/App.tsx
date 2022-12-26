6ef4494ad9708ae1e8e0aefadbb83b0b8e2d20d7
insoojang <insoo2846@gmail.com>
2022-12-26 23:20:49 +0900

    [Add] 서비스 기본로직 개발

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { getMoviesUpcoming } from './service/api'
 
 function App() {
-  return <div className="App">test</div>
+  return (
+    <div className="App">
+      <button
+        type="button"
+        onClick={() => {
+          getMoviesUpcoming(1).then((r) => {
+            console.log(r)
+          })
+        }}
+      >
+        hello2
+      </button>
+    </div>
+  )
 }
 
 export default App
---
+++ src/service/api.ts
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY
+const BASE_URL = 'https://api.themoviedb.org/3'
+
+interface IMovie {
+  id: number
+  backdrop_path: string
+  poster_path: string
+  title: string
+  overview: string
+  release_date: string
+  vote_average: string
+}
+
+export interface IGetMoviesResult {
+  result: IMovie[]
+}
+
+export const getMoviesUpcoming = async (
+  number: number,
+): Promise<IGetMoviesResult> => {
+  const { data } = await axios.get(
+    `${BASE_URL}/movie/upcoming?api_key=${REACT_APP_API_KEY}&page=${number}`,
+  )
+  return await data
+}
