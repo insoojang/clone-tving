a5899afcaf4618c55fac2c0378e410dc9392944b
insoojang <insoo2846@gmail.com>
2022-12-29 21:22:59 +0900

    [Fix] Home화면 화살표 클릭시 애니메이션 적용

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,43 +1,94 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useState } from 'react'
 import { useQuery } from 'react-query'
 import { getMoviesUpcoming, IGetMoviesResult, IMovie } from '../service/api'
 import { makeImagePath, nothingPoster } from '../service/utils'
+import { AnimatePresence, motion } from 'framer-motion'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
+
+export const rowVars = {
+  start: {
+    opacity: 1,
+  },
+  visible: { opacity: 1 },
+  exit: { opacity: 0 },
+}
 
 const Home = () => {
   const { isLoading, data } = useQuery<IGetMoviesResult>('upcoming', () =>
     getMoviesUpcoming(1),
   )
-  console.log('data', data)
+  const [index, setIndex] = useState(0)
+  const increaseIndex = () => {
+    if (data) {
+      const total = 5
+      setIndex((prev) => (prev === total ? 0 : prev + 1))
+    }
+  }
+  const decreaseIndex = () => {
+    if (data) {
+      const total = 0
+      setIndex((prev) => (prev === total ? 5 : prev - 1))
+    }
+  }
+
   return (
     <div className="w-screen h-screen bg-black">
       {isLoading ? (
         'loading'
       ) : (
-        <div className="w-full h-[calc(100vh-4rem)] grid grid-cols-10 absolute bottom-0 bg-red-200">
-          {data?.results.map((movie: IMovie) => {
-            return (
-              <Poster
-                key={movie.id}
-                bgimg={
-                  movie.backdrop_path === null
-                    ? nothingPoster
-                    : makeImagePath(movie.poster_path)
-                }
-                className="bg-slate-500 image bg-cover bg-center"
+        <div className="w-full h-[500px]">
+          <div className=" w-full  relative flex justify-center ">
+            <AnimatePresence initial={false}>
+              <motion.div
+                variants={rowVars}
+                initial="start"
+                animate="visible"
+                exit="exit"
+                transition={{ type: 'tween', duration: 1 }}
+                key={index}
+                className="grid w-full absolute top-16 opacity-90 overflow-hidden"
               >
-                <span className="text-white">{movie.title}</span>
-              </Poster>
-            )
-          })}
+                {data?.results.slice(index, index + 1).map((movie: IMovie) => {
+                  return (
+                    <div className="relative">
+                      <motion.img
+                        className=" w-full h-[calc(100vh-12rem)]"
+                        key={index}
+                        initial="normal"
+                        transition={{ type: 'tween' }}
+                        src={makeImagePath(
+                          movie.backdrop_path === null
+                            ? nothingPoster
+                            : movie.backdrop_path,
+                        )}
+                      />
+                      <div className="w-full h-full absolute flex justify-between items-center top-0 ">
+                        <motion.div
+                          whileHover={{ color: 'white' }}
+                          onClick={decreaseIndex}
+                          className="text-gray-700 z-10 w-1/2 h-full flex items-center justify-start opacity-70"
+                        >
+                          <ArrowBackIosIcon fontSize="large" />
+                        </motion.div>
+                        <motion.div
+                          whileHover={{ color: 'white' }}
+                          onClick={increaseIndex}
+                          className="text-gray-700 z-10 w-1/2 h-full flex items-center justify-end origin-right"
+                        >
+                          <ArrowForwardIosIcon fontSize="large" />
+                        </motion.div>
+                      </div>
+                    </div>
+                  )
+                })}
+              </motion.div>
+            </AnimatePresence>
+          </div>
         </div>
       )}
     </div>
   )
 }
-
-const Poster = styled.div<{ bgimg: string }>`
-  background-image: url(${(prop) => prop.bgimg});
-`
 
 export default Home
